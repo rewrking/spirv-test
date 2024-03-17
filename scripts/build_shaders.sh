354ea35a8062caeeeaf47dc5bfca213f62857d24
Andrew King <pepsiotaku@gmail.com>
2024-03-17 13:07:54 -0400

    change header name declaration

--- scripts/build_shaders.sh
+++ scripts/build_shaders.sh
@@ -4,13 +4,13 @@ src_dir=src
 build_dir=build
 output_ext=spv
 header_ext=h
-header_shader_name=main_shader
 
 glsl_vert_frag() {
 	target_env=$1
 	shader_file=$2
 	shader_input_files="$src_dir/$shader_file.vert.glsl $src_dir/$shader_file.frag.glsl"
 	output_dir=$build_dir/$target_env
+	header_shader_name=${shader_file}_shader_bin
 
 	mkdir -p $output_dir
 	glslang --target-env $target_env -o $output_dir/$shader_file.$output_ext $shader_input_files
